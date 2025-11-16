bfc47ffa3a0a78cf2f4e6f549de15a90b4cfa77e
PeraltaRomeroFacundoRodolfo <facundoperalta217@gmail.com>
2025-11-16 16:09:13 -0300

    .

---
+++ MiWebApp/Controllers/PresupuestoController.cs
@@ -0,0 +1,251 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace MiWebApp.Controllers;
+
+using MiWebApp.Repositorios;
+using MiWebApp.ViewModels;
+using MiWebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+public class PresupuestoController : Controller
+{
+    private PresupuestosRepository presu;
+    private ProductoRepositorio producto;
+    public PresupuestoController()
+    {
+        presu = new PresupuestosRepository();
+        producto = new ProductoRepositorio();
+    }
+
+
+
+
+
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<Presupuestos> presupuestos = presu.GetAllPresupuestos();
+        List<PresupuestoViewModel> presupuestosVM = presupuestos.Select(p => new PresupuestoViewModel(p)).ToList();
+        return View(presupuestosVM);
+
+    }
+
+
+    /*<input type="hidden" asp-for="IdPresupuesto" />  agregar luego*/
+
+
+    /*public IActionResult Details(int id)
+    {
+
+
+        return View(new PresupuestoViewModel(id));
+    }asi si funciona*/
+
+
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        List<Presupuestos> presupuestos = presu.GetAllPresupuestos();
+        var aux = presupuestos.FirstOrDefault(p => p.IdPresupuesto == id);
+
+
+
+        return View(new PresupuestoViewModel(aux));
+    }
+
+    [HttpPost]
+
+    public IActionResult Details(PresupuestoViewModel pr)
+    {
+        var aux = presu.obtenerPresupuestoPorId(pr.IdPresupuesto);
+        var aux1 = new PresupuestoViewModel(aux);
+        return View(aux1);
+
+    }
+
+    //razor no distingue entre mayus y  minus
+
+    [HttpGet]
+
+    public IActionResult Create()
+    {
+        List<Presupuestos> presupuestos = presu.GetAllPresupuestos();
+        int idDeUltimo = presupuestos.LastOrDefault().IdPresupuesto;
+        var pvm = new PresupuestoViewModel(idDeUltimo + 1);
+        pvm.FechaCreacion = DateTime.Now;
+        return View(pvm);
+    }
+
+
+    [HttpPost]
+
+    public IActionResult Create(PresupuestoViewModel presupuestoVM)
+    {
+
+        if (!(presupuestoVM.FechaCreacion <= DateTime.Now))
+        {
+            ModelState.AddModelError("FechaCreacion", "La fecha no puede ser posterior a hoy");
+
+
+        }
+
+
+        if (!ModelState.IsValid)
+        {
+            // vuelve a la vista, mostrando los errores
+            return View(presupuestoVM);
+        }
+
+
+        Presupuestos presupuesto = new Presupuestos(presupuestoVM);
+
+        presu.CrearPresupuesto(presupuesto);
+        return RedirectToAction("Index");
+
+
+
+    }
+
+
+
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        List<Presupuestos> presupuestos = presu.GetAllPresupuestos();
+        var presuEncontrado = presupuestos.FirstOrDefault(p => p.IdPresupuesto == id);
+
+        if (presuEncontrado == null)
+            return NotFound();
+
+
+        var vm = new PresupuestoViewModel(presuEncontrado);
+        return View(vm);
+    }
+
+
+
+    [HttpPost]
+    public IActionResult Edit(PresupuestoViewModel presupuesto)
+    {
+
+        if (!(presupuesto.FechaCreacion <= DateTime.Now))
+        {
+            ModelState.AddModelError("FechaCreacion", "La fecha no puede ser posterior a hoy");
+
+
+        }
+        if (!ModelState.IsValid)
+        {
+            return View(presupuesto);
+        }
+
+         
+        var presup = new Presupuestos(presupuesto);
+        presu.ActualizarPresupuesto(presup.IdPresupuesto, presup);
+        return RedirectToAction("Index");
+    }
+
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+
+        return View(new PresupuestoViewModel(id));
+    }
+
+
+
+    [HttpPost]
+    public IActionResult Delete(PresupuestoViewModel presupuesto)
+    {
+        
+        presu.borrarPresupuesto(presupuesto.IdPresupuesto);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult AgregarProducto(int id)
+    {
+        List<Productos> productos = producto.GetAll();
+
+        AgregarProductoViewModel model = new AgregarProductoViewModel()
+        {
+            IdPresupuesto = id, // Pasamos el ID del presupuesto actual
+                                // 3. Crear el SelectList
+            ListaProductos = new SelectList(productos, "IdProducto", "Descripcion")
+        };
+
+        return View(model);
+    }
+
+
+
+    [HttpPost]
+    public IActionResult AgregarProducto(AgregarProductoViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            List<Productos> productos = producto.GetAll();
+            model.ListaProductos = new SelectList(productos, "IdProducto", "Descripcion");
+            return View(model);
+
+        }
+
+        if (presu.GetAllPresupuestos().Exists(p=>p.IdPresupuesto==model.IdPresupuesto && p.Detalle.Exists(p1=>p1.Producto.IdProducto==model.IdProducto)))
+        {
+           var auxPresu= presu.GetAllPresupuestos().FirstOrDefault(p=>p.IdPresupuesto==model.IdPresupuesto);
+        
+           var cant= auxPresu.cantidadDeUnProducto(model.IdProducto)+ model.Cantidad;
+            presu.actualizarCantidad(model.IdPresupuesto, model.IdProducto, cant);
+        }else
+        {
+            presu.AgregarProductoAPresupuesto(model.IdPresupuesto, model.IdProducto, model.Cantidad);
+        }        
+        
+        return RedirectToAction("Details", new { id = model.IdPresupuesto });
+    }
+
+
+
+    [HttpGet]
+
+    public IActionResult EditCantidad(int idProducto, int idPresupuesto)
+    {
+        List<Productos> productos = producto.GetAll();
+
+        var model=new AgregarProductoViewModel(idProducto, idPresupuesto);
+
+        return View(model);
+        
+    }
+
+    [HttpPost]
+    public IActionResult EditCantidad(AgregarProductoViewModel model)
+    {
+
+        if (!ModelState.IsValid)
+        {   
+            ModelState.AddModelError("IdProducto", "Falta producto");
+            ModelState.AddModelError("IdPresupuesto", "Falta presupuesto");
+            return View(model);
+        }
+
+        presu.actualizarCantidad(model.IdPresupuesto,model.IdProducto, model.Cantidad);
+        return RedirectToAction("Details", new { id = model.IdPresupuesto });
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}//asp-route-idProducto
+//osea porque route se llama idProducto entonces en el get debe llamarse asi?
+//Sí, exactamente.
+//En ASP.NET Core MVC, los nombres de las variables que vos pasás con asp-route-xxxx deben coincidir con los nombres de los parámetros del método GET. Si no coinciden, el valor no se enlaza y te llega 0, null, o el valor por defecto.
---
+++ MiWebApp/Controllers/ProductosController.cs
@@ -0,0 +1,167 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+
+namespace MiWebApp.Controllers;
+
+
+using MiWebApp.Models;
+using MiWebApp.Repositorios;
+using MiWebApp.ViewModels;
+
+
+public class ProductosController : Controller
+{
+
+
+    private ProductoRepositorio producto;
+    public ProductosController()
+    {
+        producto = new ProductoRepositorio();
+    }
+
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        List<Productos> productos = producto.GetAll();
+        List<ProductoViewModel> prodVM = productos.Select(p => new ProductoViewModel(p)).ToList();
+        return View(prodVM);
+
+    }
+
+
+
+    [HttpGet]
+
+    public IActionResult Details(int id)
+    {
+
+        var aux = producto.GetAll();
+        var aux1 = aux.FirstOrDefault(p => p.IdProducto == id);
+
+        return View(new ProductoViewModel(aux1));
+    }
+
+
+    [HttpPost]
+
+    public IActionResult Details(ProductoViewModel prodVM)
+    {
+        /*List<Productos> productos = producto.GetAll();
+        var produc=productos.FirstOrDefault(p=>p.IdProducto==prodVM.IdProducto);*/
+
+
+
+        var aux = producto.ObtenerProductoPorNombre(prodVM.IdProducto);
+        var aux1 = new ProductoViewModel(aux);
+
+        return View(aux1);
+
+    }
+
+    //razor no distingue entre mayus y  minus
+
+    [HttpGet]
+
+    public IActionResult Create()
+    {
+        List<Productos> productos = producto.GetAll();
+        int idDeUltimo = productos.LastOrDefault().IdProducto;
+        return View(new ProductoViewModel(idDeUltimo + 1));
+
+    }
+
+
+    [HttpPost]
+
+    public IActionResult Create(ProductoViewModel producVM)
+    {
+
+
+
+        if (!ModelState.IsValid)
+        {
+            // vuelve a la vista, mostrando los errores
+            return View(producVM);
+        }
+
+        var produ = new Productos(producVM);
+        producto.InsertarProducto(produ);
+        return RedirectToAction("Index");
+
+    }
+
+
+
+
+    /*    [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            return View(new ProductoViewModel(id));
+        }
+
+    sin data en el form
+    */
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        List<Productos> productos = producto.GetAll();
+        var productoEncontrado = productos.FirstOrDefault(p => p.IdProducto == id);
+
+        if (productoEncontrado == null)
+            return NotFound();
+
+        var vm = new ProductoViewModel(productoEncontrado);
+        return View(vm);
+    }
+
+
+    [HttpPost]
+    public IActionResult Edit(ProductoViewModel producVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            // vuelve a la vista, mostrando los errores
+            return View(producVM);
+        }
+
+        
+        var produ = new Productos(producVM);
+
+        producto.ActualizarProducto(produ.IdProducto, produ);
+        return RedirectToAction("Index");
+    }
+
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+
+        return View(new ProductoViewModel(id));
+    }
+
+
+
+    [HttpPost]
+    public IActionResult Delete(ProductoViewModel producVM)
+    {
+        
+        producto.borrarProducto(producVM.IdProducto);
+        return RedirectToAction("Index");
+    }
+
+
+
+
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
